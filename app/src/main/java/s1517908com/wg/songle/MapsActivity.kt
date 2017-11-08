81b22d54c3a1e3535d92a5847be6bd21fb042c17
s1517908 <s1517908@sms.ed.ac.uk>
2017-11-08 19:19:37 +0000

    Initial commit

--- app/src/main/java/s1517908com/wg/songle/MapsActivity.kt
+++ app/src/main/java/s1517908com/wg/songle/MapsActivity.kt
@@ -10,9 +10,25 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.LocationListener
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener{
 
     private lateinit var mMap: GoogleMap
+    private lateinit var mGoogleApiClient: GoogleApiClient
+    val permissionsRequestAccessFineLocation = 1
+    var mLocationPermissionGranted = false
+    // getLastLocation can return null, so we need the type ”Location?”
+    private var mLastLocation : android.location.Location? = null
+    val tag = "MapsActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +37,13 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        // Create an instance of GoogleAPIClient.
+        mGoogleApiClient = GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build()
     }
 
     /**
@@ -36,8 +59,78 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val FHill = LatLng(55.946233, -3.192473)
+        mMap.addMarker(MarkerOptions().position(FHill).title("FHill"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(FHill))
+
+        try {  // Visualise current position with a small blue circle
+            mMap.isMyLocationEnabled = true
+        } catch (se : SecurityException) {
+            println("Security exception thrown [onMapReady]")
+        }
+            // Add ”My location” button to the user interface
+            mMap.uiSettings.isMyLocationButtonEnabled = true
     }
+
+    override fun onStart() { super.onStart()
+        mGoogleApiClient.connect() }
+    override fun onStop() { super.onStop()
+        if (mGoogleApiClient.isConnected) { mGoogleApiClient.disconnect()
+        } }
+
+    fun createLocationRequest() {
+        // Set the parameters for the location request
+        val mLocationRequest = LocationRequest()
+        mLocationRequest.interval = 5000 // preferably every 5 seconds
+        mLocationRequest.fastestInterval = 1000 // at most every second
+        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        // Can we access the user’s current location?
+        val permissionCheck = android.support.v4.content.ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+
+        if (permissionCheck == android.content.pm.PackageManager.PERMISSION_GRANTED) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(
+                    mGoogleApiClient, mLocationRequest, this)
+        }
+    }
+
+    override fun onConnected(connectionHint : Bundle?) {
+        try {
+        createLocationRequest()
+        }   catch (ise : IllegalStateException) {
+        println("[$tag] [onConnected] IllegalStateException thrown") }
+        // Can we access the user’s current location?
+        if (android.support.v4.content.ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == android.content.pm.PackageManager.PERMISSION_GRANTED) {
+            val api = LocationServices.FusedLocationApi
+            mLastLocation = api.getLastLocation(mGoogleApiClient)
+            // Caution: getLastLocation can return null
+            if (mLastLocation == null) {
+                println("[$tag] Warning: mLastLocation is null")
+            } }
+        else {
+            android.support.v4.app.ActivityCompat.requestPermissions(this,
+                    arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                    permissionsRequestAccessFineLocation)
+        } }
+    override fun onLocationChanged(current : android.location.Location?) {
+        if (current == null) {
+            println("[$tag] [onLocationChanged] Location unknown") }
+        else {
+            println("""[$tag] [onLocationChanged] Lat/long now
+             (${current.latitude},
+            ${current.longitude})"""
+            )
+        // Do something with current location
+
+    } }
+
+    override fun onConnectionSuspended(flag : Int) {
+        println(" >>>> onConnectionSuspended")
+    }
+    override fun onConnectionFailed(result : ConnectionResult) {
+    // An unresolvable error has occurred and a connection to Google APIs // could not be established. Display an error message, or handle
+    // the failure silently
+        println(" >>>> onConnectionFailed")
+    }
+
 }
