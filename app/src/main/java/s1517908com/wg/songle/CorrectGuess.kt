5af95e23db42b3fce1d7d4799c00879422e33542
s1517908 <s1517908@sms.ed.ac.uk>
2017-11-10 00:09:56 +0000

    Correct guess XML added

--- app/src/main/java/s1517908com/wg/songle/CorrectGuess.kt
+++ app/src/main/java/s1517908com/wg/songle/CorrectGuess.kt
@@ -4,20 +4,17 @@ import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 
-import kotlinx.android.synthetic.main.activity_auto_complete_page.*
-
+import kotlinx.android.synthetic.main.activity_correct_guess.*
 import android.view.View
 
-
-class AutoCompletePage : AppCompatActivity() {
+class CorrectGuess : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auto_complete_page)
+        setContentView(R.layout.activity_correct_guess)
         setSupportActionBar(toolbar)
 
         fab.setVisibility(View.GONE)
-
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
