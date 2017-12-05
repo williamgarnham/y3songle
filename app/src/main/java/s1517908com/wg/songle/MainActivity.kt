524b307ae91d5c6d88dedceb67a753cdaa9e96b4
s1517908 <s1517908@sms.ed.ac.uk>
2017-12-05 19:08:43 +0000

    Buttons from main activity go to correct activites

--- app/src/main/java/s1517908com/wg/songle/MainActivity.kt
+++ app/src/main/java/s1517908com/wg/songle/MainActivity.kt
@@ -5,11 +5,17 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 import android.content.Intent
+import android.widget.Button
+
 
 
 
 class MainActivity : AppCompatActivity() {
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,17 +23,35 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
 
-        /*fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
-
-
-        kotlinx.android.synthetic.main.activity_main.button8.setOnClickListener {
+        completedSongs.setOnClickListener { view ->
             val intent = Intent(this, CompletedSongs::class.java)
-            startActivity(intent)
+            startActivity(intent);
         }
-        */
+        contButton.setOnClickListener { view ->
+            val intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent);
+        }
+        easyButton.setOnClickListener { view ->
+            val intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent);
+        }
+        interButton.setOnClickListener { view ->
+            val intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent);
+        }
+        hardButton.setOnClickListener { view ->
+            val intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent);
+        }
+        expertButton.setOnClickListener { view ->
+            val intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent);
+        }
+        legendaryButton.setOnClickListener { view ->
+            val intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent);
+        }
+
     }
 
 
